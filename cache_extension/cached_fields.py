d39dac89c6c54fc2c4346bb1e7c620bd534bf964
Legolas Bloom <zhanhsw@gmail.com>
2017-04-10 14:25:32 +0800

    add cache_exc arg instead

--- cache_extension/cached_fields.py
+++ cache_extension/cached_fields.py
@@ -5,21 +5,27 @@ from django.utils.functional import curry
 
 class CachedModel(models.Model):
 
-    def _cached_FIELD(self, field):
+    def _cached_FIELD(self, field, cache_exc):
         value = getattr(self, field.attname)
         Model = field.related_model
-        return cache.get_model(Model, pk=value, cache_exc=True)
+        return cache.get_model(Model, pk=value, cache_exc=cache_exc)
 
     class Meta:
         abstract = True
 
 
 class ForeignKeyField(models.ForeignKey):
+
+    def __init__(self, *args, **kwargs):
+        self.cache_exc = kwargs.pop('cache_exc', False)
+        super(ForeignKeyField, self).__init__(*args, **kwargs)
+
     def contribute_to_class(self, cls, name, **kwargs):
         super(models.ForeignKey, self).contribute_to_class(cls, name, **kwargs)
         if issubclass(cls, models.Model):
             setattr(cls, "cached_%s" % self.name,
-                    property(curry(CachedModel._cached_FIELD, field=self)))
+                    property(curry(CachedModel._cached_FIELD, field=self,
+                                   cache_exc=self.cache_exc)))
 
 
 class OneToOneField(models.OneToOneField, ForeignKeyField):
