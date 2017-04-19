6da09b9b2950c8d81fe45b51fc9c02c587ad5a1d
shellfly <shell0fly@gmail.com>
2017-04-19 18:07:54 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-cache-extension',
-    version='1.1.9',
+    version='1.1.10',
     description='cache extension for django',
     url='https://github.com/Beeblio/django-cache-extension',
     author='Shanbay python dev group',
